6b5e4b84a9b2346d72a836f9d6ad118cecbe5fc6
jmgeo <j.mills.geo@gmail.com>
2021-07-12 21:54:00 -0500

    doing something else.

--- to_html.py
+++
@@ -1,8 +0,0 @@
-#import dependencies
-import pandas as pd
-
-#import csv file into pandas
-cities_data = pd.read_csv("resources/cities.csv")
-
-#use .to_html to convert
-cities_data.to_html("cities.html", index=False)
